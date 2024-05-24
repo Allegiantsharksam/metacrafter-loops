b78097bea4cd8f3d095d969533a7939d485b7e7d
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-05-24 17:24:54 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 let nftcs=[];
-function NFTM(name, creator, description, dateCreated, attributes){
+function mintNFT(name, creator, description, dateCreated, attributes){
   const nft = {
     name:name,
     creator:creator,
@@ -23,19 +23,19 @@ function NFTM(name, creator, description, dateCreated, attributes){
     console.log("Attributes: "+JSON.stringify(nft.attributes, null, 2));
   });
 }
-function FTSY() {
+function getTotalSupply() {
   return nftcs.length;
 }
-NFTM("GTA", "Rockstar", "A unique CryptoPunk", "2022-07-05",{ rarity: "Rare", color: "Blue" });
-NFTM("Bored Ape", "Yuga Labs", "A bored ape yacht club NFT", "2024-01-02",{ mood: "Bored", accessory: "Hat" });
-NFTM("Art Blocks", "Art Blocks Inc", "A generative art NFT", "2024-01-03",{ algorithm: "Fractal", theme: "Abstract" });
+mintNFT("GTA", "Rockstar", "A unique CryptoPunk", "2022-07-05",{ rarity: "Rare", color: "Blue" });
+mintNFT("Bored Ape", "Yuga Labs", "A bored ape yacht club NFT", "2024-01-02",{ mood: "Bored", accessory: "Hat" });
+mintNFT("Art Blocks", "Art Blocks Inc", "A generative art NFT", "2024-01-03",{ algorithm: "Fractal", theme: "Abstract" });
 console.log("\nList of all NFTs:");
 listNFTs();
 console.log("\nTotal number of NFTs minted:");
-console.log(FTSY());
-NFTM("Meebit", "Larva Labs", "A 3D voxel character", "2024-01-04", { type: "Voxel", character: "Humanoid" });
-NFTM("World of Women", "WoW Foundation", "Celebrating representation and inclusivity", "2024-01-05", { empowerment: "High", diversity: "Wide" });
+console.log(getTotalSupply());
+mintNFT("Meebit", "Larva Labs", "A 3D voxel character", "2024-01-04", { type: "Voxel", character: "Humanoid" });
+mintNFT("World of Women", "WoW Foundation", "Celebrating representation and inclusivity", "2024-01-05", { empowerment: "High", diversity: "Wide" });
 console.log("\nUpdated list of all NFTs:");
 listNFTs();
 console.log("\nTotal number of NFTs minted:");
-console.log(FTSY());
+console.log(getTotalSupply());
