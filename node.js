2103feaf4410b153424a2ac84955a7e128cefd8f
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-05-24 17:34:45 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,10 +1,11 @@
 let nftcs=[];
-function mintNFT(name, creator, description, dateCreated){
+function mintNFT(name, creator, description, dateCreated, attributes){
   const nft = {
     name:name,
     creator:creator,
     description:description,
-    dateCreated:dateCreated
+    dateCreated:dateCreated,
+     attributes: attributes
   };
   nftcs.push(nft);
   console.log(`Minted:${name}`);
@@ -24,14 +25,14 @@ function mintNFT(name, creator, description, dateCreated){
 function getTotalSupply() {
   return nftcs.length;
 }
-mintNFT("GTA", "Rockstar", "A unique CryptoPunk", "2022-07-05");
-mintNFT("Bored Ape", "Yuga Labs", "A bored ape yacht club NFT", "2024-01-02");
+mintNFT("GTA", "Rockstar", "A unique CryptoPunk", "2022-07-05", { rarity: "Rare", color: "Blue" });
+mintNFT("Bored Ape", "Yuga Labs", "A bored ape yacht club NFT", "2024-01-02", { rarity: "Rare", color: "Blue" });
 console.log("\nList of all NFTs:");
 listNFTs();
 console.log("\nTotal number of NFTs minted:");
 console.log(getTotalSupply());
-mintNFT("Meebit", "Larva Labs", "A 3D voxel character", "2024-01-04");
-mintNFT("World of Women", "WoW Foundation", "Celebrating representation and inclusivity", "2024-01-05");
+mintNFT("Meebit", "Larva Labs", "A 3D voxel character", "2024-01-04", { rarity: "Rare", color: "Blue" });
+mintNFT("World of Women", "WoW Foundation", "Celebrating representation and inclusivity", "2024-01-05", { rarity: "Rare", color: "Blue" });
 console.log("\nUpdated list of all NFTs:");
 listNFTs();
 console.log("\nTotal number of NFTs minted:");
