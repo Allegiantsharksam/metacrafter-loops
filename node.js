4c729626e7dac409298d116ff8725214811749df
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-05-24 14:34:21 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 let nftcs = [];
-function mintNFT(name, creator, description, dateCreated, attributes) {
+function NFTM(name, creator, description, dateCreated, attributes) {
   const nft = {
     name: name,
     creator: creator,
@@ -29,9 +29,9 @@ function listNFTs() {
 function getTotalSupply() {
   return nftcs.length;
 }
-mintNFT("CryptoPunk", "Larva Labs", "A unique CryptoPunk", "2024-01-01", { rarity: "Rare", color: "Blue" });
-mintNFT("Bored Ape", "Yuga Labs", "A bored ape yacht club NFT", "2024-01-02", { mood: "Bored", accessory: "Hat" });
-mintNFT("Art Blocks", "Art Blocks Inc", "A generative art NFT", "2024-01-03", { algorithm: "Fractal", theme: "Abstract" });
+NFTM("GTA", "Rockstar", "A unique CryptoPunk", "2022-07-05", { rarity: "Rare", color: "Blue" });
+NFTM("Bored Ape", "Yuga Labs", "A bored ape yacht club NFT", "2024-01-02", { mood: "Bored", accessory: "Hat" });
+NFTM("Art Blocks", "Art Blocks Inc", "A generative art NFT", "2024-01-03", { algorithm: "Fractal", theme: "Abstract" });
 
 console.log("\nList of all NFTs:");
 listNFTs();
@@ -39,11 +39,11 @@ listNFTs();
 console.log("\nTotal number of NFTs minted:");
 console.log(getTotalSupply());
 
-mintNFT("Meebit", "Larva Labs", "A 3D voxel character", "2024-01-04", { type: "Voxel", character: "Humanoid" });
-mintNFT("World of Women", "WoW Foundation", "Celebrating representation and inclusivity", "2024-01-05", { empowerment: "High", diversity: "Wide" });
+NFTM("Meebit", "Larva Labs", "A 3D voxel character", "2024-01-04", { type: "Voxel", character: "Humanoid" });
+NFTM("World of Women", "WoW Foundation", "Celebrating representation and inclusivity", "2024-01-05", { empowerment: "High", diversity: "Wide" });
 
 console.log("\nUpdated list of all NFTs:");
 listNFTs();
 
-console.log("\nUpdated total number of NFTs minted:");
+console.log("\nTotal number of NFTs minted:");
 console.log(getTotalSupply());
