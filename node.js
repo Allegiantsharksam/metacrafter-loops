23d1fd9c72f88d745a249b5a4e3661f19c5248dc
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-05-25 09:02:09 +0530

    Update node.js

--- node.js
+++ node.js
@@ -5,15 +5,22 @@ function mintNFT(name, creator, description, dateCreated, attributes){
     creator:creator,
     description:description,
     dateCreated:dateCreated,
-     attributes: attributes
+     attributes:attributes
   };
+
+  
   nftcs.push(nft);
   console.log(`Minted:${name}`);
-}function listNFTs(){
+}
+
+
+function listNFTs(){
   if (nftcs.length===0) {
     console.log("No NFT has been minted yet.");
     return;
   }
+
+  
   nftcs.forEach((nft, index)=>{
     console.log(`NFT${index+1}:`);
     console.log("Name: "+nft.name);
@@ -22,17 +29,22 @@ function mintNFT(name, creator, description, dateCreated, attributes){
     console.log("Date Created: "+nft.dateCreated);
   });
 }
+
+
 function getTotalSupply() {
   return nftcs.length;
 }
 
-mintNFT("GTA", "Rockstar", "A unique CryptoPunk", "2022-07-05", { rarity: "Rare", color: "Blue" });
-mintNFT("Bored Ape", "Yuga Labs", "A bored ape yacht club NFT", "2024-01-02", { rarity: "Rare", color: "Blue" });
+
+mintNFT("GTA", "Rockstar", "A unique Cryptogame", "2022-07-05",{rarity:"Rare", color: "Gold"});
+mintNFT("Watch dogs", "Sleeping dogs", "A cyber NFT", "2021-09-08",{ rarity:"Legendary", color: "Blue"});
 console.log("\nList of all NFTs:");
 listNFTs();
 console.log("\nTotal number of NFTs minted:", getTotalSupply())
-mintNFT("Meebit", "Larva Labs", "A 3D voxel character", "2024-01-04", { rarity: "Rare", color: "Blue" });
-mintNFT("World of Women", "WoW Foundation", "Celebrating representation and inclusivity", "2024-01-05", { rarity: "Rare", color: "Blue" });
+
+
+mintNFT("COC", "Supercell", "A phone NFT", "2022-05-21",{ rarity:"Common",color: "Green"});
+mintNFT("Avengers", "Marvels", "Superpowered NFT", "2020-11-25",{rarity: "Legendary",color: "Red"});
 console.log("\nUpdated list of all NFTs:");
 listNFTs();
 console.log("\nTotal number of NFTs minted:", getTotalSupply())
